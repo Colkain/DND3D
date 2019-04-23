de534eb99baf20f27f1936d4d8d3fe942fbed5fb
Colkain <47830370+Colkain@users.noreply.github.com>
2019-04-23 21:30:26 +0100

    Added all potion ressources

--- DnD3D/Assets/Scripts/Item.cs
+++ DnD3D/Assets/Scripts/Item.cs
@@ -24,28 +24,24 @@ public class Item {
             requirements[1] = 3;
             effects[7] = 2;
             description = "Requirements:\n\tStrength:3\n\tAgility:3\nEffect:\n\t+2 Damage";
-            icon = Resources.Load ("Sword") as Sprite;
         } else if (id == 1) {
             typeI = "Equipment";
             name = "Dagger";
             requirements[1] = 5;
             effects[7] = 4;
             description = "Requirements:\n\tAgility:5\nEffect:\n\t+4 Damage";
-            icon = Resources.Load ("Dagger") as Sprite;
         } else if (id == 2) {
             typeI = "Equipment";
             name = "Greatsword";
             requirements[0] = 5;
             effects[7] = 4;
             description = "Requirements:\n\tStrength:5\nEffect:\n\t+4 Damage";
-            icon = Resources.Load ("Greatsword") as Sprite;
         } else if (id == 3) {
             typeI = "Equipment";
             name = "Staff";
             requirements[2] = 5;
             effects[7] = 4;
             description = "Requirements:\n\tIntelligence:5\nEffect:\n\t+4 Damage";
-            icon = Resources.Load ("Staff") as Sprite;
         } else if (id == 4) {
             typeI = "Equipment";
             name = "Wand";
@@ -53,14 +49,12 @@ public class Item {
             requirements[3] = 3;
             effects[7] = 2;
             description = "Requirements:\n\tIntelligence:3\n\tWisdom:3\nEffect:\n\t+2 Damage";
-            icon = Resources.Load ("Wand") as Sprite;
         } else if (id == 5) {
             typeI = "Equipment";
             name = "Mace";
             requirements[3] = 5;
             effects[7] = 4;
             description = "Requirements:\n\tWisdom:5\nEffect:\n\t+4 Damage";
-            icon = Resources.Load ("Mace") as Sprite;
         } else if (id == 6) {
             typeI = "Equipment";
             name = "Bow";
@@ -68,7 +62,6 @@ public class Item {
             requirements[3] = 3;
             effects[8] = 1;
             description = "Requirements:\n\tStrength:3\n\tWisdom:3\nEffect:\n\t+2 Damage";
-            icon = Resources.Load ("Bow") as Sprite;
         } else if (id == 7) {
             typeI = "Equipment";
             name = "Crossbow";
@@ -76,7 +69,6 @@ public class Item {
             requirements[1] = 3;
             effects[8] = 1;
             description = "Requirements:\n\tIntelligence:3\n\tAgility:3\nEffect:\n\t+2 Damage";
-            icon = Resources.Load ("Crossbow") as Sprite;
         } else if (id == 8) {
             typeI = "Equipment";
             name = "Light Armor";
@@ -86,7 +78,6 @@ public class Item {
             requirements[3] = 2;
             effects[1] = 2;
             description = "Requirements:\n\tStrength:2\n\tAgility:2\n\tIntelligence:2\n\tWisdom:2\nEffect:\n\t+2 MaxHealth";
-            icon = Resources.Load ("Light Armor") as Sprite;
         } else if (id == 9) {
             typeI = "Equipment";
             name = "Medium Armor";
@@ -96,7 +87,6 @@ public class Item {
             requirements[3] = 4;
             effects[1] = 4;
             description = "Requirements:\n\tStrength:4\n\tAgility:4\n\tIntelligence:4\n\tWisdom:4\nEffect:\n\t+4 MaxHealth";
-            icon = Resources.Load ("Medium Armor") as Sprite;
         } else if (id == 10) {
             typeI = "Equipment";
             name = "Heavy Armor";
@@ -106,43 +96,36 @@ public class Item {
             requirements[3] = 6;
             effects[1] = 6;
             description = "Requirements:\n\tStrength:6\n\tAgility:6\n\tIntelligence:6\n\tWisdom:6\nEffect:\n\t+6 MaxHealth";
-            icon = Resources.Load ("Heavy Armor") as Sprite;
         } else if (id == 11) {
             typeI = "Potion";
             name = "Health Potion";
             effects[2] = 2;
             description = "Effect:\n\t+2 Health";
-            icon = Resources.Load ("Health Potion") as Sprite;
         } else if (id == 12) {
             typeI = "Potion";
             name = "Mouvement Potion";
             effects[0] = 2;
             description = "Effect:\n\t+2 Mouvement";
-            icon = Resources.Load ("Mouvement Potion") as Sprite;
         } else if (id == 13) {
             typeI = "Potion";
             name = "Strength Potion";
             effects[3] = 2;
             description = "Effect:\n\t+2 Strength";
-            icon = Resources.Load ("Strength Potion") as Sprite;
         } else if (id == 14) {
             typeI = "Potion";
             name = "Agility Potion";
             effects[4] = 2;
             description = "Effect:\n\t+2 Agility";
-            icon = Resources.Load ("Agility Potion") as Sprite;
         } else if (id == 15) {
             typeI = "Potion";
             name = "Intelligence Potion";
             effects[5] = 2;
             description = "Effect:\n\t+2 Intelligence";
-            icon = Resources.Load ("Intelligence Potion") as Sprite;
         } else if (id == 16) {
             typeI = "Potion";
             name = "Wisdom Potion";
             effects[6] = 2;
             description = "Effect:\n\t+2 Wisdom";
-            icon = Resources.Load ("Wisdom Potion") as Sprite;
         } else if (id == 17) {
             typeI = "Potion";
             name = "All Stats Potion";
@@ -154,9 +137,10 @@ public class Item {
             effects[5] = 1;
             effects[6] = 1;
             description = "Effects:\n\t+2 Health\n+2 Max Health\n+2 Mouvement\n+2 Strength\n+2 Agility\n+2 Intelligence\n+2 Wisdom";
-            icon = Resources.Load ("All Stats Potion") as Sprite;
         } else
             Debug.Log ("Item Error " + id);
+
+        icon = Resources.Load (name) as Sprite;
     }
     public string GetTypeI () => typeI;
     public string GetName () => name;
