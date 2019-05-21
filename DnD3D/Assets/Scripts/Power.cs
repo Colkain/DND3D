2cb7e3692ec796ab3d0ffe081deaef8031332ba5
Colkain <47830370+Colkain@users.noreply.github.com>
2019-05-21 17:36:12 +0100

    Update Power.cs

--- DnD3D/Assets/Scripts/Power.cs
+++ DnD3D/Assets/Scripts/Power.cs
@@ -17,145 +17,155 @@ public class Power {
         level = 1;
         cooldownUI = 0;
         if (id == 0) {
-            intensity = level;
             name = "Berserk";
-            description = "+" + intensity + " Action.";
             cooldown = 3;
             duration = 1;
         } else if (id == 1) {
-            intensity = level;
             name = "Sprint";
-            description = "+" + intensity + " Movement.";
             cooldown = 3;
             duration = 1;
         } else if (id == 2) {
-            intensity = level;
             name = "Snipe";
-            description = "+" + intensity + " Range.";
             cooldown = 3;
             duration = 1;
         } else if (id == 3) {
-            intensity = 2 + level;
             name = "Heal";
-            description = "Can Heal: " + level + ";" + intensity;
             cooldown = 3;
             duration = 1;
         } else if (id == 4) {
-            intensity = 1 + level;
             name = "Damage";
-            description = "+" + intensity + " bonus damage.";
             cooldown = 3;
             duration = 1;
         } else if (id == 5) {
             name = "Block";
-            duration = level;
-            description = "Prevent damage for " + duration + " turns.";
             cooldown = 3;
         } else if (id == 6) {
             name = "Refresh";
-            description = "Reset power cooldowns.";
-            cooldown = 6 - level;
             duration = 1;
         } else if (id == 7) {
-            intensity = 5 - level;
-            if (intensity == 0)
-                intensity++;
             name = "Limit Break: Damage";
-            description = "Decrease your health by " + intensity + " yourself and increase your damage by 1.";
             cooldown = 0;
             duration = 1;
         } else if (id == 8) {
             name = "Limit Break: Movement";
-            intensity = 5 - level;
-            if (intensity == 0)
-                intensity++;
-            description = "Decrease your health by " + intensity + " yourself and increase your movement by 1.";
             cooldown = 0;
             duration = 1;
         } else if (id == 9) {
             name = "Limit Break: Action";
-            intensity = 6 - level;
-            if (intensity == 1)
-                intensity++;
-            description = "Decrease your health by " + intensity + " yourself and gain 1 action.";
             cooldown = 0;
             duration = 1;
         } else if (id == 10) {
-            intensity = level;
             name = "Adventurer: Reckless";
-            description = "Deal " + intensity + " damage to everyone in the tile you are entering.";
             passive = true;
         } else if (id == 11) {
-            intensity = level;
             name = "Adventurer: Careful";
-            description = "Gain " + intensity + " health when entering a tile.";
             passive = true;
         } else if (id == 12) {
-            intensity = level;
             name = "Explorer: Careful";
-            description = "Gain " + intensity + " health when exploring a new tile.";
             passive = true;
         } else if (id == 13) {
-            intensity = level;
             name = "Explorer: Impatient";
-            description = "Gain " + intensity + " movement when exploring a new tile.";
             passive = true;
         } else if (id == 14) {
-            intensity = level;
             name = "Vampire";
-            description = "Gain " + intensity + " health when attacking.";
             passive = true;
         } else if (id == 15) {
             name = "Leech";
-            intensity = 10 * level;
-            if (intensity > 100)
-                intensity = 100;
-            description = "Gain health equal to " + intensity + "% of your damage when attacking.";
             passive = true;
         } else if (id == 16) {
-            intensity = level;
             name = "Investigator: Action";
-            description = "Gain " + intensity + " action when checking a new tile";
             cooldown = 3;
         } else if (id == 17) {
-            intensity = level;
             name = "Investigator: Movement";
-            description = "Gain " + intensity + " movement when checking a new tile";
             cooldown = 2;
         } else if (id == 18) {
-            intensity = level;
             name = "Investigator: Health";
-            description = "Gain " + intensity + " health when checking a new tile";
             cooldown = 2;
         } else if (id == 19) {
-            intensity = level;
             name = "Investigator: Damage";
-            description = "Gain " + intensity + " damage when checking a new tile";
             cooldown = 2;
         } else if (id == 20) {
-            intensity = level;
             name = "Investigator: Max Health";
-            description = "Gain " + intensity + " max health when checking a new tile";
             cooldown = 3;
         }
+        SetPower ();
         icon = Resources.Load<Sprite> (name);
     }
     public string GetName () => name;
     public string GetDescription () => description;
-    public void SetDescription () {
-        if (id == 0)
-            description = "+" + level + " Action.";
-        else if (id == 1)
-            description = "+" + level + " Mouvement.";
-        else if (id == 2)
-            description = "+" + level + " Range.";
-        else if (id == 3) {
-            int ia = 4 + level;
-            description = "Can Heal: " + level + ";" + ia;
-        } else if (id == 4)
-            description = "+" + level + " bonus damage.";
-        else if (id == 5)
-            description = "Prevent damage for " + level + " turns.";
+    public void SetPower () {
+        if (id == 0) {
+            intensity = level;
+            description = "+" + intensity + " Action.";
+        } else if (id == 1) {
+            intensity = level;
+            description = "+" + intensity + " Movement.";
+        } else if (id == 2) {
+            intensity = level;
+            description = "+" + intensity + " Range.";
+        } else if (id == 3) {
+            intensity = 2 + level;
+            description = "Can Heal: " + level + ";" + intensity;
+        } else if (id == 4) {
+            intensity = 1 + level;
+            description = "+" + intensity + " bonus damage.";
+        } else if (id == 5) {
+            duration = level;
+            description = "Prevent damage for " + duration + " turns.";
+        } else if (id == 6) {
+            description = "Reset power cooldowns.";
+            cooldown = 6 - level;
+        } else if (id == 7) {
+            intensity = 5 - level;
+            if (intensity == 0)
+                intensity++;
+            description = "Decrease your health by " + intensity + " yourself and increase your damage by 1.";
+        } else if (id == 8) {
+            intensity = 5 - level;
+            if (intensity == 0)
+                intensity++;
+            description = "Decrease your health by " + intensity + " yourself and increase your movement by 1.";
+        } else if (id == 9) {
+            intensity = 6 - level;
+            if (intensity == 1)
+                intensity++;
+            description = "Decrease your health by " + intensity + " yourself and gain 1 action.";
+        } else if (id == 10) {
+            intensity = level;
+            description = "Deal " + intensity + " damage to everyone in the tile you are entering.";
+        } else if (id == 11) {
+            intensity = level;
+            description = "Gain " + intensity + " health when entering a tile.";
+        } else if (id == 12) {
+            intensity = level;
+            description = "Gain " + intensity + " health when exploring a new tile.";
+        } else if (id == 13) {
+            intensity = level;
+            description = "Gain " + intensity + " movement when exploring a new tile.";
+        } else if (id == 14) {
+            intensity = level;
+            description = "Gain " + intensity + " health when attacking.";
+        } else if (id == 15) {
+            intensity = 10 * level;
+            if (intensity > 100)
+                intensity = 100;
+            description = "Gain health equal to " + intensity + "% of your damage when attacking.";
+        } else if (id == 16) {
+            intensity = level;
+            description = "Gain " + intensity + " action when checking a new tile";
+        } else if (id == 17) {
+            intensity = level;
+            description = "Gain " + intensity + " movement when checking a new tile";
+        } else if (id == 18) {
+            intensity = level;
+            description = "Gain " + intensity + " health when checking a new tile";
+        } else if (id == 19) {
+            intensity = level;
+            description = "Gain " + intensity + " damage when checking a new tile";
+        } else if (id == 20) {
+            intensity = level;
+            description = "Gain " + intensity + " max health when checking a new tile";
+        }
     }
     public int GetIntensity () => intensity;
     public void SetIntensity (int i) {
@@ -170,9 +180,8 @@ public class Power {
     public int GetLevel () => level;
     public void SetLevel (int i) {
         level += i;
-        if (id == 5)
-            duration += i;
-        SetDescription ();
+        SetPower ();
+        Debug.Log (name + " " + level + " " + intensity);
     }
     public int GetDuration () => duration;
     public Sprite GetIcon () => icon;
